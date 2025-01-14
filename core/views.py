28308f32c6d697a43cc62961aa9a262b385978ac
Gaurav Sonkar <142399666+Gaurav-SonkaR@users.noreply.github.com>
2025-01-15 00:13:33 +0530

    Delete core directory

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home_page(request):
-    return render(request,"index.html")
-
-def products_page(request):
-    return render(request,"products.html")
